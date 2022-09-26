bef62fe1a3d6caa5f5dcd35dcc919ea456a46741
igorkotof <igor.kotov@hazelops.com>
2022-09-26 13:45:12 +0300

    Added support NLB

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
-  name             = "${var.env}-${var.name}"
+  name             = var.app_type == "tcp-app" ? "${var.env}-${var.name}-tcp" : "${var.env}-${var.name}"
   ecs_service_name = var.ecs_service_name != "" ? var.ecs_service_name : "${var.env}-${var.name}"
   ecs_cluster_name = var.ecs_cluster_name != "" ? var.ecs_cluster_name : "${var.env}-${var.namespace}"
   ecs_cluster_arn  = length(var.ecs_cluster_arn) != "" ? var.ecs_cluster_arn : "arn:aws:ecs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:cluster/${local.ecs_cluster_name}"
-  ecr_repo_name    = var.ecr_repo_name != "" ? var.ecr_repo_name : "${var.namespace}-${var.name}"
+  ecr_repo_name    = var.ecr_repo_name != "" ? var.ecr_repo_name : "${var.env}-${var.namespace}-${var.name}"
   name_prefix      = "${substr(var.name, 0, 5)}-"
   domain_names     = var.root_domain_name != "example.com" ? concat(["${var.name}.${var.env}.${var.root_domain_name}"], var.domain_names) : []
 
@@ -102,4 +102,75 @@ locals {
     ] : [],
     (var.datadog_enabled && var.ecs_launch_type == "EC2") ? module.datadog.volumes : []
   )
+
+  http_tcp_listeners = var.app_type == "tcp-app" ? [
+    for index, port_mapping in var.port_mappings :
+      {
+        port               = port_mapping.host_port
+        protocol           = "TCP"
+        target_group_index = index
+      }
+    ] : [
+      {
+        port               = var.http_port
+        protocol           = "HTTP"
+        target_group_index = 0
+      },]
+
+  ecs_service_tcp_port_mappings = [
+    for index, port_mapping in var.port_mappings :
+      {
+        container_name      = var.name
+        container_port      = port_mapping.container_port
+        host_port           = port_mapping.host_port
+        target_group_arn    = length(module.alb[*].target_group_arns) >= 1 ? module.alb[0].target_group_arns[index] : ""
+      }
+    ]
+
+  target_groups_web = [
+    {
+      name_prefix          = local.name_prefix
+      backend_protocol     = "HTTP"
+      backend_port         = var.web_proxy_enabled ? var.web_proxy_docker_container_port : var.docker_container_port
+      target_type          = var.ecs_launch_type == "EC2" ? "instance" : "ip"
+      deregistration_delay = var.alb_deregistration_delay
+
+      health_check = {
+        enabled             = true
+        interval            = var.alb_health_check_interval
+        path                = var.alb_health_check_path
+        healthy_threshold   = var.alb_health_check_healthy_threshold
+        unhealthy_threshold = var.alb_health_check_unhealthy_threshold
+        timeout             = var.alb_health_check_timeout
+        matcher             = var.alb_health_check_valid_response_codes
+        port                = "traffic-port"
+        protocol            = "HTTP"
+      }
+    }
+  ]
+
+  target_groups_tcp = [
+    for port_mapping in var.port_mappings :
+    {
+      name_prefix          = local.name_prefix
+      backend_protocol     = "TCP"
+      backend_port         = port_mapping.container_port
+      target_type          = var.ecs_launch_type == "EC2" ? "instance" : "ip"
+      deregistration_delay = var.alb_deregistration_delay
+
+      health_check = {
+        enabled             = true
+        interval            = var.alb_health_check_interval
+        path                = null
+        healthy_threshold   = var.alb_health_check_healthy_threshold
+        unhealthy_threshold = var.alb_health_check_unhealthy_threshold
+        timeout             = null
+        matcher             = null
+        port                = port_mapping.host_port
+        protocol            = "TCP"
+      }
+
+    }
+  ]
+  
 }
