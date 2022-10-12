6ee718e3bfdb366b93af1b21f0c7d00445122cb4
Dmitry Borisov <dmitry.borisov@hazelops.com>
2022-10-12 17:20:00 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "alb" {
 
 module "ecr" {
   source  = "registry.terraform.io/hazelops/ecr/aws"
-  version = "~> 1.0"
+  version = "~> 1.1"
 
   name         = local.ecr_repo_name
   enabled      = var.ecr_repo_create
