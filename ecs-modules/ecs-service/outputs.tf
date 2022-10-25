01b1a4dbf5f75e8a5bacd914eae40f9e17244ffc
igorkotof <igor.kotov@hazelops.com>
2022-10-25 12:32:26 +0300

    added worker tests

--- ecs-modules/ecs-service/outputs.tf
+++ ecs-modules/ecs-service/outputs.tf
@@ -12,3 +12,8 @@ output "cloudwatch_log_group" {
   description = "Cloudwatch Log group of ECS Service"
   value       = module.task.cloudwatch_log_group
 }
+
+output "cloudwatch_event_rule_id" {
+  description = "Cloudwatch event rule for ECS Scheduled Task"
+  value       = (var.app_type == "worker" && length(var.cloudwatch_schedule_expressions)>0) ? aws_cloudwatch_event_rule.this[0].id : "" 
+}
--- examples/complete-worker/versions.tf
+++ examples/complete-worker/versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
+      #version = "~> 3.0,!= 3.14.0"
     }
   }
   required_version = ">= 1.0"
