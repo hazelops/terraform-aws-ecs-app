2847cc5604b30a0b2faae158cc76e4cdc04c6cd0
Igor Kotov <igor.kotof@gmail.com>
2022-10-28 17:19:52 +0300

    Update examples/complete-web/main.tf
    Co-authored-by: Dmitry Kireev <dmitry@hazelops.com>

--- examples/complete-web/main.tf
+++ examples/complete-web/main.tf
@@ -47,9 +47,6 @@ resource "aws_route53_record" "env_ns_record" {
   zone_id = data.aws_route53_zone.root.id
   name    = "${var.env}.${var.root_domain_name}"
   type    = "NS"
-  //  ttl  = "172800"
-
-  // Fast TTL for dev
   ttl     = "60"
   records = aws_route53_zone.env_domain.name_servers
 }
