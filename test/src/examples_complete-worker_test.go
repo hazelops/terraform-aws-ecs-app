abd9367cc523a45753bde73a2e6768c9829cc85d
igorkotof <igor.kotov@hazelops.com>
2022-11-16 17:45:04 +0300

    varFiles

--- test/src/examples_complete-worker_test.go
+++ test/src/examples_complete-worker_test.go
@@ -23,7 +23,7 @@ func TestExamplesCompleteWorker(t *testing.T) {
 
 	rootFolder := "../../"
 	terraformFolderRelativeToRoot := "examples/complete-worker"
-	varFiles := []string{"./terraform.tfvars"}
+	varFiles := []string{"../../examples/complete-worker/terraform.tfvars"}
 
 	tempTestFolder := test_structure.CopyTerraformFolderToTemp(t, rootFolder, terraformFolderRelativeToRoot)
 
