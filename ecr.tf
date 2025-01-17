c3f8bd4bb2038d4108cf71e488488f233a573a3a
Dmitry Borisov <dmitry.borisov@hazelops.com>
2025-01-17 03:52:05 +0300

    ECR module updated to v2.x

--- ecr.tf
+++ ecr.tf
@@ -1,6 +1,6 @@
 module "ecr" {
   source  = "registry.terraform.io/hazelops/ecr/aws"
-  version = "~> 1.1"
+  version = "~> 2.0"
 
   name         = local.ecr_repo_name
   enabled      = var.ecr_repo_create
